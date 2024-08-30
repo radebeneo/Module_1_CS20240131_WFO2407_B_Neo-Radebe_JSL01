8e377caed272a0806da5cfb08ef079a96005189b
umkhulubhungane <admin@Apple-MacBook-Pro-Retina-13-Early-2015.local>
2024-08-30 09:38:31 +0200

    If statement to check string begins with pet_

--- script.js
+++ script.js
@@ -5,8 +5,13 @@ function validateSyntax() {
 
     // TODO: Write your validation logic here
         // Check if input starts with 'pet_' and followed by alphanumeric characters
+        
+        // Check if input starts with 'pet_'
+        if (input.startsWith('pet_')) {
+            
+        }
 
-            document.getElementById('result').innerText = result;
+    document.getElementById('result').innerText = result;
 }
 
 
